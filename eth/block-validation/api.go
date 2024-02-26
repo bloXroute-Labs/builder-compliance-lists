62309f49ddb9f2e84bcb2f2cdfda8d8b4bea2cb2
jzblox <106985178+jzblox@users.noreply.github.com>
2024-02-26 15:20:25 -0500

    add log line

--- eth/block-validation/api.go
+++ eth/block-validation/api.go
@@ -286,6 +286,7 @@ func (api *BlockValidationAPI) validateBlock(block *types.Block, msg *builderApi
 	accessVerifier := api.accessVerifier
 
 	if complianceList != "" {
+		log.Info("using compliance list in block validation", "list", complianceList)
 		accessVerifier = NewAccessVerifierWithComplianceList(complianceList)
 	}
 
